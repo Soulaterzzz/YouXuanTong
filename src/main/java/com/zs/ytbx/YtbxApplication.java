77115cffc613e688d74466a5522aac0f116eeb47
Soulaterzzz <www.2624868527@gmail.com>
2026-03-19 14:32:03 +0800

    初始化项目

---
+++ src/main/java/com/zs/ytbx/YtbxApplication.java
@@ -0,0 +1,13 @@
+package com.zs.ytbx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtbxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtbxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zs/ytbx/YtbxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zs.ytbx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtbxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
