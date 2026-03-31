da49cd0a2ba3fe5b37df6011b9b5e43e26c8ce2b
Soulaterzzz <www.2624868527@gmail.com>
2026-03-31 15:45:04 +0800

    剔除前后端冗余代码

--- src/main/java/com/zs/ytbx/mapper/SysUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.zs.ytbx.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zs.ytbx.entity.SysUserEntity;
-
-public interface SysUserMapper extends BaseMapper<SysUserEntity> {
-}
