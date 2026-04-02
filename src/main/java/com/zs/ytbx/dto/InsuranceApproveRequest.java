cb85566d5d3e971d6a596b3400a579c24680120b
Soulaterzzz <www.2624868527@gmail.com>
2026-04-02 22:42:26 +0800

    Revert "完善订单审核细节"
    This reverts commit 98615d91a138395d990d9f624d210b72bc3d7865.

--- src/main/java/com/zs/ytbx/dto/InsuranceApproveRequest.java
+++ src/main/java/com/zs/ytbx/dto/InsuranceApproveRequest.java
@@ -3,17 +3,9 @@ package com.zs.ytbx.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-import java.time.LocalDate;
-
 @Data
 public class InsuranceApproveRequest {
 
     @NotBlank(message = "审核意见不能为空")
     private String reviewComment;
-
-    private String policyNo;
-
-    private LocalDate effectiveDate;
-
-    private LocalDate expiryDate;
 }
